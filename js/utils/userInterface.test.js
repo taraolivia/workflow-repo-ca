3058c097e919a54e1524733ebab328a03767c469
taraolivia <151462360+taraolivia@users.noreply.github.com>
2024-12-12 16:17:22 +0100

    Testing of `isActivePath` Function. All tests are passing

--- js/utils/userInterface.test.js
+++ js/utils/userInterface.test.js
@@ -1,4 +1,4 @@
-// #### `isActivePath` Function
+// #### `isActivePath` Function. I will be testing for this:
 // 1. Returns `true` when the current path matches `href` exactly.
 // 2. Returns `true` for the root path (`"/"`) when the path is `"/"` or `"/index.html"`.
 // 3. Returns `true` when the current path includes the `href`.
@@ -14,12 +14,26 @@ describe("isActivePath", () => {
     expect(isActivePath("/index.html", "/index.html")).toBe(true);
   });
 
-  it("Returns `true` for the root path (/) when the path is `/` or `/index.html", () => {
+  it("Returns `true` for the root path (/) when the path is `/` or `/index.html`", () => {
     expect(isActivePath("/", "/")).toBe(true);
-    expect(isActivePath("/", "index.html")).toBe(true);
+    expect(isActivePath("/", "/index.html")).toBe(true);
   });
 
-  it("Returns `true` when the current path includes the `href`.", () => {});
+  it("Returns `true` when the current path includes the `href`.", () => {
+    //for home page
+    expect(isActivePath("/", "/index.html")).toBe(true);
+    expect(isActivePath("/", "/")).toBe(true);
+    //for venue page
+    expect(isActivePath("/venue", "/venue/index.html")).toBe(true);
+    //for register page
+    expect(isActivePath("/register", "/register/index.html")).toBe(true);
+    //for login page
+    expect(isActivePath("/login", "/login/index.html")).toBe(true);
+  });
 
-  it("Returns `false` when paths don't match.", () => {});
+  it("Returns `false` when paths don't match.", () => {
+    expect(isActivePath("/register", "/login/index.html")).toBe(false);
+    expect(isActivePath("/venue", "/register/index.html")).toBe(false);
+    expect(isActivePath("/", "/login/index.html")).toBe(false);
+  });
 });
